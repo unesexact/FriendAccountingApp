4d103c0802f8c9ae5442520741d5ccdabf48da76
unesexact <oukessouyounes1@gmail.com>
2026-02-26 15:03:17 +0000

    feat: implement Add Friend dialog with Material3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -62,6 +62,7 @@ dependencies {
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
+    implementation(libs.androidx.material3)
     implementation(libs.androidx.compose.runtime.livedata)
 
     debugImplementation(libs.androidx.compose.ui.tooling)
